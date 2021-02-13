04ab245cb9b791bed948b825c4a5aa02565f4da3
Andres Ortiz <andres.ortiz@hydra.cloud>
2021-02-13 08:33:06 -0500

    Project initialization

---
+++ communications/src/test/java/com/challenge/quasarfire/communications/CommunicationsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.challenge.quasarfire.communications;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommunicationsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
